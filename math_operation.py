022d5f19d1bc4ad85ca75746f02c8c7e43ebc719
den <medenis91@gmail.com>
2024-10-29 18:41:20 +0100

    add math_operation file

---
+++ math_operation.py
@@ -0,0 +1,11 @@
+ivan_marks = 10
+maria_marks = 5
+
+print("Марія мала", maria_marks, 'конвертиків, а Іван', ivan_marks, 'разом у них було', maria_marks, '+', ivan_marks,
+      '=', maria_marks + ivan_marks)
+# ctrl + alt + l
+
+print('First element', 'Second element', 3, 4)
+print('First element', 'Second element', 3, 4, sep=' ')
+print('First element', 'Second element', 3, 4, sep=' ||| ', end='\n')
+print('First element', 'Second element', 3, 4, sep=' ||| ', end='\nend of print block\n')
