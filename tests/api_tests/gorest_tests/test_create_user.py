ad9c7c24187d01c54c5beaa881654fbb3e03ffe9
den <medenis91@gmail.com>
2025-02-28 20:09:26 +0100

    added new tests

--- tests/api_tests/gorest_tests/test_create_user.py
+++ tests/api_tests/gorest_tests/test_create_user.py
@@ -1,9 +1,12 @@
+import os
 import time
 
 import allure
 
 from setting import d_settings
 from tests.api_tests.gorest_tests.base_gorest import BaseGorestTest
+
+from assertpy import assert_that, soft_assertions
 
 
 @allure.story("Create user")
@@ -29,5 +32,15 @@ class TestCreateUser(BaseGorestTest):
     def test_work_with_env(self):
 
         assert d_settings.DATABASE_URL == 'postgresql://den:den@localhost:5432/new_db'
+
+
+    @allure.severity(allure.severity_level.CRITICAL)
+    @allure.title('Additional test for working with env vars')
+    def test_work_with_env(self):
+
+        with soft_assertions():
+            assert_that(d_settings.runner, 'checking d_settings.runner').is_equal_to('jenkins')
+
+            assert_that(os.getenv('custom_env_var', None), 'checnkign env var custom_env_var').is_equal_to('jenkins_env_var')
 
 
