624652cf1052e62d9178b0f38ac9e13921d6f0c2
den <medenis91@gmail.com>
2025-02-28 19:48:23 +0100

    add extra test

--- tests/api_tests/gorest_tests/test_create_user.py
+++ tests/api_tests/gorest_tests/test_create_user.py
@@ -2,6 +2,7 @@ import time
 
 import allure
 
+from setting import d_settings
 from tests.api_tests.gorest_tests.base_gorest import BaseGorestTest
 
 
@@ -21,5 +22,12 @@ class TestCreateUser(BaseGorestTest):
         }
 
         self.gorest_ctrl.create_user(user_body=user_body, headers={'User-Agent': 'Custom-user-agent'})
+
+
+    @allure.severity(allure.severity_level.CRITICAL)
+    @allure.title('Additional test for working with .env')
+    def test_work_with_env(self):
+
+        assert d_settings.DATABASE_URL == 'postgresql://den:den@localhost:5432/new_db'
 
 
